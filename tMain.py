0a4191ebcebd363614f607b1e127fad365fbf67a
戴俊浩 <181250022@smail.nju.edu.cn>
2020-04-05 11:05:34 +0800

    虎牙重播
    更新虎牙的重播的点播功能

--- tMain.py
+++ tMain.py
@@ -13,9 +13,14 @@ def get_m3u():
         index_json = load_dict["index"]
         for i in range(0, length):
             url = all_url(index_json[i]["kind"], index_json[i]["rid"])
-            if url.startswith("http"):
+            if url.startswith("http") and not url.endswith("重播中"):
                 m3u = m3u + "#EXTINF:-1," + index_json[i]["name"] + "---" + index_json[i]["kind"] + "\r\n" + url + "\r\n"
                 notic = notic + "\r\n" + index_json[i]["name"] + ": " + "开播啦q(≧▽≦q)"
+            elif url.endswith("重播中"):
+                url = url[0:len(url) - 3]
+                m3u = m3u + "#EXTINF:-1," + index_json[i]["name"] + "---" + index_json[i][
+                    "kind"] + "\r\n" + url + "\r\n"
+                notic = notic + "\r\n" + index_json[i]["name"] + ": " + "重播中ヾ(≧▽≦*)o"
             else:
                 notic = notic + "\r\n" + index_json[i]["name"] + ": " + "未开播/(ㄒoㄒ)/~~"
             # 清屏
