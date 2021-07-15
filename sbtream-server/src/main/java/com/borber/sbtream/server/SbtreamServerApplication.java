824c19ad46513f4ea65c1f153aa735ccadd6ca3d
BORBER <borber.drum@gmail.com>
2021-07-15 15:29:04 +0800

    [更新] 添加 sbtream-server 模块

---
+++ sbtream-server/src/main/java/com/borber/sbtream/server/SbtreamServerApplication.java
@@ -0,0 +1,13 @@
+package com.borber.sbtream.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbtreamServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbtreamServerApplication.class, args);
+    }
+
+}
---
+++ sbtream-server/src/test/java/com/borber/sbtream/server/SbtreamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borber.sbtream.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbtreamServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
