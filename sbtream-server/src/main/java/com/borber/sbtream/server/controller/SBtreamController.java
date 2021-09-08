148321045a8436d7e8ca20a55c329747803f2e92
BORBER <30563826+Borber@users.noreply.github.com>
2021-09-08 16:54:53 +0800

    [mod] 添加 返回信息接口, 获取直播状态接口

--- sbtream-server/src/main/java/com/borber/sbtream/server/controller/SBtreamController.java
+++ sbtream-server/src/main/java/com/borber/sbtream/server/controller/SBtreamController.java
@@ -1,4 +1,38 @@
 package com.borber.sbtream.server.controller;
 
+import com.borber.sbtream.api.plugin.response.BaseResponse;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
 public class SBtreamController {
+    /**
+     * #TODO 获取指定平台指定房间号的直播状态/地址
+     */
+    @PostMapping("/{pid}/{rid}")
+    public BaseResponse getRealUrl(@PathVariable String pid, @PathVariable String rid){
+        return null;
+    }
+
+    /**
+     * #TODO 返回平台信息
+     * @param pid 平台代号
+     * @return json
+     */
+    @GetMapping("/{pid}")
+    public BaseResponse getPlatform(@PathVariable String pid){
+        return null;
+    }
+
+    /**
+     * #TODO 返回主播信息
+     * @param pid 平台代号
+     * @param rid 房间号
+     * @return json
+     */
+    @GetMapping("/{pid}/{rid}")
+    public BaseResponse getAnchor(@PathVariable String pid, @PathVariable String rid){
+        return null;
+    }
+
 }
