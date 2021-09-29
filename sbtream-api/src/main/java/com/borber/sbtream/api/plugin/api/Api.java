24c13436da3043879d9d7d965b3d0a302add6862
BORBER <borber.drum@gmail.com>
2021-09-29 14:15:09 +0800

    [doc] 更新api注释说明

--- sbtream-api/src/main/java/com/borber/sbtream/api/plugin/api/Api.java
+++ sbtream-api/src/main/java/com/borber/sbtream/api/plugin/api/Api.java
@@ -11,18 +11,20 @@ package com.borber.sbtream.api.plugin.api;
 public interface Api {
     /**
      * 获取直播平台元数据
-     * @Author Borber
-     * @Date 2021/7/15 上午11:28
-     * @Param []
+     * @author Borber
+     * @date 2021/7/15 上午11:28
+     * @param pid 平台代号
      * @return String 但是必须是 BaseResponse<PlatformDTO, Object> 的 序列化字符串
      **/
     String getMetaData(String pid);
 
     /**
      * 通过房间号获取真实直播地址  如果是需要 Cookies 来获取即使用, 如果不需要则忽略 cookies 变量
-     * @Author Borber
-     * @Date 2021/7/15 下午10:54
-     * @Param [rid, cookies]
+     * @author Borber
+     * @date 2021/7/15 下午10:54
+     * @param pid 平台代号
+     * @param rid 房间号
+     * @param cookies 用户认证
      * @return String 但是必须是 BaseResponse<UrlDTO, Object> 的 序列化字符串
      **/
     String getRealUrl(String pid, String rid, String cookies);
@@ -30,9 +32,12 @@ public interface Api {
     /**
      *
      * 通过cookies发送弹幕, 返回是否成功
-     * @Author Borber
-     * @Date  2021/09/08 下午03:50
-     * @Param pid 平台代号, rid 主播房间号, str 弹幕内容, cookies 用户认证
+     * @author Borber
+     * @date  2021/09/08 下午03:50
+     * @param pid 平台代号
+     * @param rid 主播房间号
+     * @param str 弹幕内容
+     * @param cookies 用户认证
      * @return String 但是必须是 BaseResponse<Boolean, Object> 的 序列化字符串
      */
     String sendDanmaku(String pid, String rid, String str, String cookies);
