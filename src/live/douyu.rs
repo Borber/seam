b2448534557c825f13248b0b9c167ef927391a7d
Borber <borber.drum@gmail.com>
2023-01-06 21:45:41 +0800

    [mod] 原创斗鱼地址获取

--- src/live/douyu.rs
+++ src/live/douyu.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 
+use crate::modle::Node;
 use crate::{common::CLIENT, modle::ShowType};
 
 use crate::util::md5;
@@ -8,6 +9,11 @@ use chrono::prelude::*;
 use regex::Regex;
 use reqwest::header::HeaderMap;
 
+const URL: &str = "https://www.douyu.com/";
+const M_URL: &str = "https://m.douyu.com/";
+const PLAY_URL: &str = "https://playweb.douyucdn.cn/lapi/live/hlsH5Preview/";
+const CDN_1: &str = "http://akm-tct.douyucdn.cn/live/";
+const CDN_2: &str = "http://ws-tct.douyucdn.cn/live/";
 const DID: &str = "10000000000000000000000000001501";
 
 /// 斗鱼直播
@@ -15,24 +21,34 @@ const DID: &str = "10000000000000000000000000001501";
 /// https://www.douyu.com/
 pub async fn get(rid: &str) -> Result<ShowType> {
     let dt = Local::now().timestamp_millis().to_string();
-    let rid = real_rid(rid).await;
-    get_pre(&rid, dt).await;
-    Ok(ShowType::Off)
+    let rid = match real_rid(rid).await {
+        Some(rid) => rid,
+        None => return Ok(ShowType::Error("直播间不存在".to_string())),
+    };
+    let key = match get_pre(&rid, dt).await {
+        Some(key) => key,
+        None => return Ok(ShowType::Off),
+    };
+    Ok(ShowType::On(vec![Node{
+        rate: "超清1".to_string(),
+        url: format!("{CDN_1}{key}.flv"),
+    }, Node{
+        rate: "超清2".to_string(),
+        url: format!("{CDN_2}{key}.flv"),
+    }]))
 }
 
-async fn real_rid(rid: &str) -> String {
-    let resp = CLIENT
-        .get(format!("https://m.douyu.com/{rid}"))
-        .send()
-        .await
-        .unwrap();
+async fn real_rid(rid: &str) -> Option<String> {
+    let resp = CLIENT.get(format!("{M_URL}{rid}")).send().await.unwrap();
     let text = resp.text().await.unwrap();
     let re = Regex::new(r#"rid":(\d{1,8}),"vipId"#).unwrap();
-    let caps = re.captures(&text).unwrap();
-    caps.get(1).unwrap().as_str().to_owned()
+    match re.captures(&text) {
+        Some(caps) => Some(caps.get(1).unwrap().as_str().to_owned()),
+        None => None,
+    }
 }
-async fn get_pre(rid: &str, dt: String) {
-    let url = format!("https://playweb.douyucdn.cn/lapi/live/hlsH5Preview/{rid}");
+async fn get_pre(rid: &str, dt: String) -> Option<String> {
+    let url = format!("{PLAY_URL}{rid}");
     let auth = md5(format!("{rid}{dt}").as_bytes());
 
     let mut headers = HeaderMap::new();
@@ -54,7 +70,11 @@ async fn get_pre(rid: &str, dt: String) {
         .json()
         .await
         .unwrap();
-    println!("{}", resp);
+    // 因为前面以及判断过直播间是否存在, 所以现在直接判断是否开播, 不做 102, 104 状态码的区分
+    match resp["error"].to_string().parse::<usize>().unwrap() {
+        0 => Some(resp["data"]["rtmp_live"].to_string().trim_matches('"').split_once("_").unwrap().0.to_owned()),
+        _ => None
+    }
 }
 
 #[cfg(test)]
