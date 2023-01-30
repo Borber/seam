65412acd77a799ed4afea41780e9618cfd9a9fcf
Borber <borber.drum@gmail.com>
2023-01-30 14:40:40 +0800

    [mod] 修改ci脚本, 更新依赖

---
+++ build/build-host-release.ps1
@@ -0,0 +1,60 @@
+#!pwsh
+<#
+    OpenSSL is already installed on windows-latest virtual environment.
+    If you need OpenSSL, consider install it by:
+
+    choco install openssl
+#>
+param(
+    [Parameter(HelpMessage = "extra features")]
+    [Alias('f')]
+    [string]$Features
+)
+
+$ErrorActionPreference = "Stop"
+
+$TargetTriple = (rustc -Vv | Select-String -Pattern "host: (.*)" | ForEach-Object { $_.Matches.Value }).split()[-1]
+
+Write-Host "Started building release for ${TargetTriple} ..."
+
+if ([string]::IsNullOrEmpty($Features)) {
+    cargo build --release
+}
+else {
+    cargo build --release --features "${Features}"
+}
+
+if (!$?) {
+    exit $LASTEXITCODE
+}
+
+$Version = (Select-String -Pattern '^version *= *"([^"]*)"$' -Path "${PSScriptRoot}\..\Cargo.toml" | ForEach-Object { $_.Matches.Value }).split()[-1]
+$Version = $Version -replace '"'
+
+$PackageReleasePath = "${PSScriptRoot}\release"
+$PackageName = "seam-v${Version}.${TargetTriple}.zip"
+$PackagePath = "${PackageReleasePath}\${PackageName}"
+
+Write-Host $Version
+Write-Host $PackageReleasePath
+Write-Host $PackageName
+Write-Host $PackagePath
+
+Push-Location "${PSScriptRoot}\..\target\release"
+Copy-Item "${PSScriptRoot}\..\config.json" -Destination "${PSScriptRoot}\..\target\release"
+
+$ProgressPreference = "SilentlyContinue"
+New-Item "${PackageReleasePath}" -ItemType Directory -ErrorAction SilentlyContinue
+$CompressParam = @{
+    LiteralPath     = "seam.exe", "config.json"
+    DestinationPath = "${PackagePath}"
+}
+Compress-Archive @CompressParam
+
+Write-Host "Created release packet ${PackagePath}"
+
+$PackageChecksumPath = "${PackagePath}.sha256"
+$PackageHash = (Get-FileHash -Path "${PackagePath}" -Algorithm SHA256).Hash
+"${PackageHash}  ${PackageName}" | Out-File -FilePath "${PackageChecksumPath}"
+
+Write-Host "Created release packet checksum ${PackageChecksumPath}"
