731c5fed78239761c6cd8d5b4c79dda2e608f2b8
Borber <borber.drum@gmail.com>
2023-02-20 14:55:08 +0800

    [mod] 简化代码

--- seam_status/src/common.rs
+++ seam_status/src/common.rs
@@ -1,6 +1,7 @@
 use once_cell::sync::Lazy;
 use reqwest::Client;
 
+#[allow(dead_code)]
 pub const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.54";
 
 pub static CLIENT: Lazy<Client> = Lazy::new(Client::new);
