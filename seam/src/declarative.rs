2b64e77074291167ad84793285199290a969dc1f
Borber <borber.drum@gmail.com>
2023-05-10 15:08:58 +0800

    [mod] 调整clap结构

--- seam/src/declarative.rs
+++ seam/src/declarative.rs
@@ -10,48 +10,32 @@ use seam_status::{status, StatusTrait};
 
 use crate::Cli;
 
-// 声明宏：获取直播源的command的实现
-#[derive(Debug, Subcommand)]
+#[derive(Subcommand)]
 pub enum Commands {
+    /// bilibili直播
     Bili {
         /// 房间号
         rid: String,
-        // 输出到终端的弹幕功能
-        #[arg(short = 'd')]
-        danmu: bool,
-        // 根据参数指定的文件地址输出弹幕
-        #[arg(short = 'D')]
-        config_danmu: bool,
-        // 直接录播功能
-        #[arg(short = 'r')]
-        record: bool,
-        // 自动监控录播功能
-        #[arg(short = 'R')]
-        auto_record: bool,
     },
 }
 
 // 获取直播源的实现
 pub async fn get_source_url() -> Result<()> {
-    match Cli::parse().command {
-        Commands::Bili {
-            rid,
-            danmu,
-            config_danmu,
-            mut record,
-            auto_record,
-        } => {
+    let args = Cli::parse();
+    match args.command {
+        Commands::Bili { rid } => {
+            let mut record = args.record;
             let node = live::bili::Client::get(&rid).await?;
 
             // 无参数情况下，直接输出直播源信息
-            if !(danmu || config_danmu || record || auto_record) {
+            if !(args.danmu || args.config_danmu || record || args.auto_record) {
                 println!("{}", &node.json());
                 return Ok(());
             }
 
             // 排斥参数检查
             // 1. 自动监控录播当检查到当前在直播时，应当自动开启下载任务，所以record选项此时是多余的
-            if auto_record {
+            if args.auto_record {
                 record = false;
             }
 
@@ -60,7 +44,7 @@ pub async fn get_source_url() -> Result<()> {
 
             // 处理参数-d，直接输出弹幕。
             // 由于该函数为cli层，所以出错可以直接panic。
-            if danmu {
+            if args.danmu {
                 let rid = node.rid.clone();
                 let h = tokio::spawn(async move {
                     danmu::bili::Danmu::start(&rid, vec![&Terminal::try_new(None).unwrap()])
@@ -71,7 +55,7 @@ pub async fn get_source_url() -> Result<()> {
             }
 
             // 处理参数-D，输出弹幕到指定文件。
-            if config_danmu {
+            if args.config_danmu {
                 let node_clone = node.clone();
                 let h = tokio::spawn(async move {
                     let file_name = CONFIG
@@ -116,7 +100,7 @@ pub async fn get_source_url() -> Result<()> {
             // 处理参数-R，自动监控录制直播。
             //
             // 5秒检查一次是否开启直播。 TODO 支持自定义检查间隔
-            if auto_record {
+            if args.auto_record {
                 let node_clone = node.clone();
                 let h = tokio::spawn(async move {
                     let node = node_clone;
@@ -145,7 +129,7 @@ pub async fn get_source_url() -> Result<()> {
                 thread_handlers.push(h);
             }
 
-            if auto_record {
+            if args.auto_record {
                 tokio::select! {
                     _ = async {
                         for h in thread_handlers {
--- seam/src/main.rs
+++ seam/src/main.rs
@@ -1,12 +1,11 @@
 mod declarative;
 
-
 use crate::declarative::{get_source_url, Commands};
 use anyhow::Result;
 use clap::Parser;
 
 /// 获取直播源
-#[derive(Debug, Parser)]
+#[derive(Parser)]
 #[command(name = "seam")]
 #[command(about ="
 ________ _______ _______ _______
@@ -14,8 +13,21 @@ ________ _______ _______ _______
 |__     |    ___|       |       |
 |_______|_______|___|___|__|_|__|", long_about = None)]
 struct Cli {
+    // 子命令
     #[command(subcommand)]
     command: Commands,
+    /// 直接录播功能
+    #[arg(short = 'r', global = true)]
+    record: bool,
+    /// 自动监控录播功能
+    #[arg(short = 'R', global = true)]
+    auto_record: bool,
+    /// 输出到终端的弹幕功能
+    #[arg(short = 'd', global = true)]
+    danmu: bool,
+    /// 根据参数指定的文件地址输出弹幕
+    #[arg(short = 'D', global = true)]
+    config_danmu: bool,
 }
 
 #[tokio::main]
