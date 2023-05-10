cd3c967b8703c32f062051d97ad1aba9cbecb610
Borber <borber.drum@gmail.com>
2023-05-10 11:30:27 +0800

    [doc] 修改

--- seam_status/src/lib.rs
+++ seam_status/src/lib.rs
@@ -1,14 +1,14 @@
 //! 直播状态检测相关模块。
 //!
-//! 本模块提供了标准化的直播状态检测的 async trait 以及
-//! 标准化的直播源信息和直播状态enum
+//! 本模块提供了标准化的直播状态检测的 async trait
+
+use async_trait::async_trait;
+
+use error::Result;
 
 mod common;
 pub mod error;
 pub mod status;
-
-use async_trait::async_trait;
-use error::Result;
 
 #[async_trait]
 pub trait StatusTrait {
