42e22bda4cd1e9e27bd44f09c61c112c937bb7ad
Borber <Borber.Drum@gmail.com>
2023-08-12 15:43:16 +0800

    [mod] sidebar topbar

---
+++ crates/gui/src/components/SideBar.tsx
@@ -0,0 +1,7 @@
+import '../css/SideBar.css'
+
+const SideBar = () => {
+    return <div class="side-bar">SideBar</div>
+}
+
+export default SideBar
---
+++ crates/gui/src/components/TopBar.tsx
@@ -0,0 +1,11 @@
+const TopBar = () => {
+    return (
+        <div class="top-bar">
+            <div class="top-bar-controller-macos"></div>
+            <div class="top-bar-input"></div>
+            <div class="top-bar-controller-windows"></div>
+        </div>
+    )
+}
+
+export default TopBar
