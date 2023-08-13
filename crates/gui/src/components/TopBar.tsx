2bb78b97315717dc67ebf8775158d8ebc91b11da
Borber <borber.drum@gmail.com>
2023-08-14 02:55:15 +0800

    [mod] 添加 平台选择面板

--- crates/gui/src/components/TopBar.tsx
+++ crates/gui/src/components/TopBar.tsx
@@ -1,15 +1,47 @@
+import { createSignal } from 'solid-js'
+import toast from 'solid-toast'
+import { Transition } from 'solid-transition-group'
+
 import { AddIcon, SyncIcon } from '../icon/icon'
+import Panel from './Panel'
 
 const TopBar = () => {
+    const [rid, setRid] = createSignal('')
+    const [onInput, setInput] = createSignal(false)
+    const [onPanel, setPanel] = createSignal(false)
+    const [live, setLive] = createSignal('bili')
     return (
         <div data-tauri-drag-region class="top-bar">
             <button class="top-bar-btn">
                 <SyncIcon size={18} />
             </button>
-            <input placeholder="房间号" class="top-bar-input" />
-            <button class="top-bar-btn">
+            <input
+                placeholder="房间号"
+                class="top-bar-input"
+                onFocusIn={() => {
+                    setInput(true)
+                }}
+                onFocusOut={() => {
+                    setInput(false)
+                }}
+                onInput={async (e) => {
+                    setRid(e.target.value)
+                }}
+            />
+            <button
+                class="top-bar-btn"
+                onClick={() => {
+                    console.log(live(), rid())
+                    toast.success('添加成功')
+                }}
+            >
                 <AddIcon size={16} />
             </button>
+            <Transition name="slide-fade">
+                {(onInput() || onPanel()) && (
+                    <Panel flag={setPanel} live={live} setLive={setLive} />
+                )}
+            </Transition>
         </div>
     )
 }
