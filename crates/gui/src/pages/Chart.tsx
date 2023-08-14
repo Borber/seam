a17d677c7af83a1967faf22d4fea4e4b11422e7a
Borber <borber.drum@gmail.com>
2023-08-14 21:37:29 +0800

    [mod] 统计界面平台选择菜单

--- crates/gui/src/pages/Chart.tsx
+++ crates/gui/src/pages/Chart.tsx
@@ -1,5 +1,40 @@
+import '../css/Chart.css'
+
+import { createSignal, For } from 'solid-js'
+
+import allLives from '../model/Live'
+
 const Chart = () => {
-    return <div>123</div>
+    const [selected, setSelect] = createSignal('all')
+    return (
+        <div class="chart">
+            <div class="chart-kind-container">
+                <div
+                    class="chart-kind-item"
+                    classList={{
+                        'chart-kind-item-activate': selected() === 'all',
+                    }}
+                    onClick={() => setSelect('all')}
+                >
+                    ALL
+                </div>
+                <For each={allLives()}>
+                    {(item) => (
+                        <div
+                            class="chart-kind-item"
+                            classList={{
+                                'chart-kind-item-activate':
+                                    selected() === item.cmd,
+                            }}
+                            onClick={() => setSelect(item.cmd)}
+                        >
+                            {item.name}
+                        </div>
+                    )}
+                </For>
+            </div>
+        </div>
+    )
 }
 
 export default Chart
