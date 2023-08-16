5fa50260a3a5f47163dabc26ce5d1a9896138f59
Borber <borber.drum@gmail.com>
2023-08-16 22:24:00 +0800

    [mod] 修复斗鱼

--- crates/core/src/live/douyu.rs
+++ crates/core/src/live/douyu.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use crate::{
     common::CLIENT,
@@ -10,15 +10,13 @@ use async_trait::async_trait;
 use chrono::prelude::*;
 use md5::{Digest, Md5};
 use regex::Regex;
+use serde_json::Value;
 
 use super::{Live, Node};
 
 const URL: &str = "https://www.douyu.com/";
 const M_URL: &str = "https://m.douyu.com/";
 const PLAY_URL: &str = "https://www.douyu.com/lapi/live/getH5Play/";
-const PLAY_URL_M: &str = "https://m.douyu.com/api/room/ratestream";
-const CDN_1: &str = "http://hlstct.douyucdn2.cn/dyliveflv1a/";
-const CDN_2: &str = "http://hdltctwk.douyucdn2.cn/live/";
 const DID: &str = "10000000000000000000000000001501";
 
 /// 斗鱼直播
@@ -31,7 +29,7 @@ impl Live for Client {
     // TODO 说明所需 cookie
     async fn get(&self, rid: &str, headers: Option<HashMap<String, String>>) -> Result<Node> {
         let rid = real_rid(rid).await?;
-        douyu_do_js(&rid, headers).await
+        douyu_do_js_pc(&rid, headers).await
     }
 }
 
@@ -47,7 +45,8 @@ async fn real_rid(rid: &str) -> Result<String> {
 
 // TODO 简化代码 今天状态不行, 改天再说
 #[allow(dead_code)]
-async fn douyu_do_js_pc(rid: &str) -> Result<Node> {
+async fn douyu_do_js_pc(rid: &str, headers: Option<HashMap<String, String>>) -> Result<Node> {
+    let headers = hash2header(headers);
     // 构造时间戳
     let binding = Local::now().timestamp_millis().to_string();
     let dt = &binding.as_str()[0..10];
@@ -55,6 +54,7 @@ async fn douyu_do_js_pc(rid: &str) -> Result<Node> {
     // 获取指定直播间的首页源代码, 认证的sign和直播间是绑定的
     let text = CLIENT
         .get(format!("{URL}{rid}"))
+        .headers(headers.clone())
         .send()
         .await?
         .text()
@@ -86,6 +86,7 @@ async fn douyu_do_js_pc(rid: &str) -> Result<Node> {
 
     // 获取eval实际运行的字符串
     let res = eval(&func).await;
+    let res = res.trim_matches('"');
 
     // 构建函数, 替换数值
     let res = res.replace("(function", "let ccc = function");
@@ -101,6 +102,7 @@ async fn douyu_do_js_pc(rid: &str) -> Result<Node> {
         h.update(cb);
         hex::encode(h.finalize())
     };
+
     let res = res.replace(
         "CryptoJS.MD5(cb).toString();",
         format!("\"{}\";", &rb).as_str(),
@@ -108,6 +110,7 @@ async fn douyu_do_js_pc(rid: &str) -> Result<Node> {
 
     // 运行js获取签名值
     let sign = eval(&res).await;
+    let sign = sign.trim_matches('"');
     let sign = sign.rsplit_once('=').ok_or(SeamError::NeedFix("sign"))?.1;
 
     let mut params = HashMap::new();
@@ -116,30 +119,65 @@ async fn douyu_do_js_pc(rid: &str) -> Result<Node> {
     params.insert("tt", dt);
     params.insert("sign", sign);
 
-    let json: serde_json::Value = CLIENT
+    let json = CLIENT
         .post(format!("{PLAY_URL}{rid}"))
         .form(&params)
+        .headers(headers.clone())
         .send()
         .await?
-        .json()
+        .json::<Value>()
         .await?;
+
     match json["error"]
         .as_i64()
         .ok_or(SeamError::NeedFix("error code"))?
     {
         0 => {
-            let key = json["data"]["rtmp_live"]
+            let cdns = json["data"]["cdnsWithName"]
+                .as_array()
+                .ok_or(SeamError::NeedFix("cdns"))?;
+            let cdns = cdns
+                .iter()
+                .map(|x| x["cdn"].as_str().unwrap_or("").to_owned())
+                .collect::<HashSet<_>>();
+            let rtmp_cdn = json["data"]["rtmp_cdn"]
                 .as_str()
-                .ok_or(SeamError::NeedFix("rtmp_live"))?;
-            let key = key.split_once('.').ok_or(SeamError::NeedFix("key"))?.0;
-            let key = match key.split_once('_') {
-                Some((k, _)) => k,
-                None => key,
-            };
-            let nodes = vec![
-                parse_url(format!("{CDN_1}{key}.flv")),
-                parse_url(format!("{CDN_2}{key}.flv")),
-            ];
+                .ok_or(SeamError::NeedFix("rtmp_cdn"))?
+                .to_owned();
+
+            let mut jsons = vec![json];
+
+            if headers.get("cookie").is_some() {
+                for cdn in cdns {
+                    if cdn == rtmp_cdn {
+                        continue;
+                    }
+                    let mut tmp = params.clone();
+                    let headers_tmp = headers.clone();
+                    tmp.insert("cdn", &cdn);
+
+                    let json = CLIENT
+                        .post(format!("{PLAY_URL}{rid}"))
+                        .form(&tmp)
+                        .headers(headers_tmp)
+                        .send()
+                        .await?
+                        .json::<Value>()
+                        .await?;
+
+                    jsons.push(json);
+                }
+            }
+
+            let nodes = jsons
+                .iter()
+                .map(|json| {
+                    let key = json["data"]["rtmp_live"].as_str().unwrap_or("需要修复");
+                    let url = json["data"]["rtmp_url"].as_str().unwrap_or("需要修复");
+                    parse_url(format!("{url}/{key}"))
+                })
+                .collect::<Vec<_>>();
+
             Ok(Node {
                 rid: rid.to_owned(),
                 title: "douyu".to_owned(),
@@ -150,118 +188,5 @@ async fn douyu_do_js_pc(rid: &str) -> Result<Node> {
     }
 }
 
-async fn douyu_do_js(rid: &str, headers: Option<HashMap<String, String>>) -> Result<Node> {
-    // 构造时间戳
-    let binding = Local::now().timestamp_millis().to_string();
-    let dt = &binding.as_str()[0..10];
-
-    // 获取指定直播间的首页源代码, 认证的sign和直播间是绑定的
-    let text = CLIENT
-        .get(format!("{M_URL}{rid}"))
-        .headers(hash2header(headers))
-        .send()
-        .await?
-        .text()
-        .await?;
-
-    let re = Regex::new(r#"roomName":"([\s\S]*?)""#)?;
-    // TODO 修改为默认值, 而非错误返回
-    let title = re
-        .captures(&text)
-        .ok_or(SeamError::NeedFix("title"))?
-        .get(1)
-        .ok_or(SeamError::NeedFix("title captures"))?
-        .as_str()
-        .to_owned();
-
-    // 正则匹配固定位置的js代码
-    let re = Regex::new(r"(function ub98484234.*)\s(var.*)")?;
-    let func = re
-        .captures(&text)
-        .ok_or(SeamError::NeedFix("func"))?
-        .get(0)
-        .ok_or(SeamError::NeedFix("func captures"))?
-        .as_str()
-        .to_owned();
-
-    let re = Regex::new(r#"eval.*;}"#)?;
-    let func = re.replace(&func, "strc;}");
-    let func = format!("{func}ub98484234(0,0,0)");
-    // 获取eval实际运行的字符串
-    let res = eval(&func).await;
-
-    // 构建函数, 替换数值
-    let res = res.replace("(function", "let ccc = function");
-    let res = res.replace("rt;})", &format!("rt;}};ccc({rid}, \"{DID}\", {dt});"));
-
-    let re = Regex::new(r#"v=([0-9]{12})"#)?;
-    let v = re
-        .captures(&res)
-        .ok_or(SeamError::NeedFix("v"))?
-        .get(1)
-        .ok_or(SeamError::NeedFix("v captures"))?
-        .as_str();
-
-    // 替换md5值避免js依赖
-    let cb = format!("{rid}{DID}{dt}{v}");
-    let rb = {
-        let mut h = Md5::new();
-        h.update(cb);
-        hex::encode(h.finalize())
-    };
-    let res = res.replace(
-        "CryptoJS.MD5(cb).toString();",
-        format!("\"{}\";", &rb).as_str(),
-    );
-    // 运行js获取签名值
-    let res = res.trim().trim_matches('"');
-    let sign = eval(res).await;
-    let sign = sign.trim().trim_matches('"');
-    let sign = sign
-        .rsplit_once('=')
-        .ok_or(SeamError::NeedFix("sign split"))?
-        .1;
-
-    let mut params = HashMap::new();
-    params.insert("v", v);
-    params.insert("did", DID);
-    params.insert("tt", dt);
-    params.insert("sign", sign);
-    params.insert("rid", rid);
-
-    let json: serde_json::Value = CLIENT
-        .post(PLAY_URL_M)
-        .form(&params)
-        .send()
-        .await?
-        .json()
-        .await?;
-    match json["code"]
-        .as_i64()
-        .ok_or(SeamError::NeedFix("json code"))?
-    {
-        0 => {
-            let url_origin = json["data"]["url"]
-                .as_str()
-                .ok_or(SeamError::NeedFix("data url"))?;
-            let key = url_origin
-                .rsplit_once('/')
-                .ok_or(SeamError::NeedFix("url_origin"))?
-                .1;
-            let urls = vec![
-                parse_url(url_origin.to_owned()),
-                parse_url(format!("{CDN_1}{key}").replace(".m3u8", ".flv")),
-                parse_url(format!("{CDN_2}{key}").replace(".m3u8", ".flv")),
-            ];
-            Ok(Node {
-                rid: rid.to_owned(),
-                title,
-                urls,
-            })
-        }
-        _ => Err(SeamError::None),
-    }
-}
-
 #[cfg(test)]
-macros::gen_test!(5040227);
+macros::gen_test!(100);
